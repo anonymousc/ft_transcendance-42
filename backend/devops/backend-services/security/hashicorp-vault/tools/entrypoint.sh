e9f216dcade3ab64e46f97703d93b470ae70e89f
Amine Essadiki <aessadik@c4r5p6.1337.ma>
2026-01-24 21:42:41 +0100

    feat : adding some extra work to get the vault done

--- backend/devops/backend-services/security/hashicorp-vault/tools/entrypoint.sh
+++ backend/devops/backend-services/security/hashicorp-vault/tools/entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-# vault operator init
+set -eu
 
-exec vault server -config=/run/secrets/config.hcl
+sed -i  s/port/$PORT_VAULT/g /tools/config.hcl
+
+export VAULT_ADDR="http://127.0.0.1:$PORT_VAULT"
+
+vault server -dev -dev-root-token-id=$(cat /run/secrets/vault_key) -dev-listen-address=0.0.0.0:$PORT_VAULT&
+
+VAULT_PID=$!
+
+vault policy write postgres /tools/policy.hcl
+
+vault token create -policy=postgres -format=json | grep
+
+vault kv put -mount=secret postgres username=$(openssl rand -hex 123) password=$(openssl rand -hex 123)
+
+wait $VAULT_PID
