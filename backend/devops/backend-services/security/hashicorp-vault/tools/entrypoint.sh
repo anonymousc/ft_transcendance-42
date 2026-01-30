c9035b1418a951db1377f86217e2fdb75ec47eb3
autopsy1 <aessadik@c4r5p6.1337.ma>
2026-01-30 08:49:20 +0100

    feat : get the postgres container with prisma up and running fix the issue with the frontend dockerfile integrating vault with postgres and still has some changesti be done

--- backend/devops/backend-services/security/hashicorp-vault/tools/entrypoint.sh
+++ backend/devops/backend-services/security/hashicorp-vault/tools/entrypoint.sh
@@ -4,16 +4,24 @@ set -eu
 
 export VAULT_ADDR="http://0.0.0.0:$PORT_VAULT"
 
-vault server -dev -dev-root-token-id=$(openssl rand -hex 10) -dev-listen-address=0.0.0.0:$PORT_VAULT&
+vault server -dev -dev-root-token-id=$(openssl rand -hex 10) -dev-listen-address=0.0.0.0:$PORT_VAULT > /dev/null 2>&1 &
 
 VAULT_PID=$!
 
-sleep 30s
+until curl $VAULT_ADDR --silent --output /dev/null ; do
+    echo "vault is running"
+    sleep 3s
+done
 
-vault policy write postgres /tools/policy.hcl
+vault policy write postgres /tools/policy.hcl > /dev/null 
 
 vault token create -policy=postgres -format=json > /shared/token
 
-vault kv put -mount=secret postgres username=$(openssl rand -hex 123) password=$(openssl rand -hex 123)
+vault kv put -mount=secret postgres username=$(openssl rand -hex 12) password=$(openssl rand -hex 12) > /dev/null
+
+if [ -f /shared/token ];then
+    chown 70:70 /shared/ && chown 70:70 /shared/token
+    chmod 600 /shared/token
+fi
 
 wait $VAULT_PID
