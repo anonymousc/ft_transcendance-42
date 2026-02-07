5a43ee94e9fd3e965d9252d68d8efc8687199df8
samurai0lava <samurai0lava@iouhssei.iouhssei.virtualbox.org>
2026-02-07 17:13:16 +0100

    Refactor Footer components; remove unused styles and consolidate CSS for improved structure

--- frontend/src/components/FooterLInks.tsx
+++ frontend/src/components/FooterLInks.tsx
@@ -1,50 +1,52 @@
 import { Link } from "react-router-dom";
 
 function FooterLinks() {
+    const linkClasses = "font-sans text-base font-semibold text-white no-underline transition-colors duration-300 hover:text-[#FF8C00]";
+
     return (
-        <div className="footer-links">
-            <div className="footer-column">
-                <h3 className="footer-column-title">Navigate</h3>
-                <ul className="footer-list">
-                    <li>
-                        <Link to="/">Home</Link>
+        <div className="flex gap-16 flex-wrap">
+            <div className="min-w-[120px]">
+                <h3 className="font-mono text-[0.9rem] font-normal text-[#888888] mb-4 capitalize">Navigate</h3>
+                <ul className="list-none p-0 m-0">
+                    <li className="mb-3">
+                        <Link to="/" className={linkClasses}>Home</Link>
                     </li>
-                    <li>
-                        <Link to="/about-section">About Us</Link>
+                    <li className="mb-3">
+                        <Link to="/about-section" className={linkClasses}>About Us</Link>
                     </li>
-                    <li>
-                        <Link to="/how-it-works">How it Works</Link>
+                    <li className="mb-3">
+                        <Link to="/how-it-works" className={linkClasses}>How it Works</Link>
                     </li>
-                    <li>
-                        <Link to="/features">Features</Link>
-                    </li>
-                </ul>
-            </div>
-            <div className="footer-column">
-                <h3 className="footer-column-title">Socials</h3>
-                <ul className="footer-list">
-                    <li>
-                        <a href="#">Twitter [X]</a>
-                    </li>
-                    <li>
-                        <a href="#">Instagram</a>
-                    </li>
-                    <li>
-                        <a href="#">Facebook</a>
-                    </li>
-                    <li>
-                        <a href="#">LinkedIn</a>
+                    <li className="mb-3">
+                        <Link to="/features" className={linkClasses}>Features</Link>
                     </li>
                 </ul>
             </div>
-            <div className="footer-column">
-                <h3 className="footer-column-title">Contacts</h3>
-                <ul className="footer-list">
-                    <li>
-                        <a href="mailto:rihla@gmail.ma">rihla@gmail.ma</a>
+            <div className="min-w-[120px]">
+                <h3 className="font-mono text-[0.9rem] font-normal text-[#888888] mb-4 capitalize">Socials</h3>
+                <ul className="list-none p-0 m-0">
+                    <li className="mb-3">
+                        <a href="#" className={linkClasses}>Twitter [X]</a>
                     </li>
-                    <li>
-                        <a href="tel:+212000000000">+212 000 000 000</a>
+                    <li className="mb-3">
+                        <a href="#" className={linkClasses}>Instagram</a>
+                    </li>
+                    <li className="mb-3">
+                        <a href="#" className={linkClasses}>Facebook</a>
+                    </li>
+                    <li className="mb-3">
+                        <a href="#" className={linkClasses}>LinkedIn</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="min-w-[120px]">
+                <h3 className="font-mono text-[0.9rem] font-normal text-[#888888] mb-4 capitalize">Contacts</h3>
+                <ul className="list-none p-0 m-0">
+                    <li className="mb-3">
+                        <a href="mailto:rihla@gmail.ma" className={linkClasses}>rihla@gmail.ma</a>
+                    </li>
+                    <li className="mb-3">
+                        <a href="tel:+212000000000" className={linkClasses}>+212 000 000 000</a>
                     </li>
                 </ul>
             </div>
