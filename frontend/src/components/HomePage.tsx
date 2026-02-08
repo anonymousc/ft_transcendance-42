665c0c46457358ecf65e4aa78a7062163f5ca589
samurai0lava <samurai0lava@iouhssei.iouhssei.virtualbox.org>
2026-02-08 13:23:41 +0100

    Add background video to HomePage and update styles for improved aesthetics

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -6,38 +6,34 @@ import bgImage from "../assets/imssouane.png";
 
 import largeCloud from "../assets/large-3d-cloud.png";
 import smallCloud from "../assets/small-3d-cloud.png";
+import bgvideo from "../assets/home-background.mp4";
 
 function HomePage() {
-    const handleNavigation = (id: string) => {
-        console.log("Navigate to:", id);
-    };
+  const handleNavigation = (id: string) => {
+    console.log("Navigate to:", id);
+  };
 
-    const handleSearch = (query: string) => {
-        console.log("Search query:", query);
-        
-    };
+  const handleSearch = (query: string) => {
+    console.log("Search query:", query);
+  };
 
-    return (
-        <div className="home-page" style={{ backgroundImage: `url(${bgImage})` }}>
-            <VerticalNav onNavigate={handleNavigation} />
-            {/* <h1 className="header-home">Where do you want to go next ?</h1>
-            <h1 className="sub-header">Travel smarter. Explore together.</h1> */}
-            <div className="clouds-container">
-                {/* <img src={largeCloud} alt="" className="cloud cloud-large" />
-                <img src={smallCloud} alt="" className="cloud cloud-s   mall-left" /> */}
-            </div>
-
-            <main className="home-content">
-                <div className="search-container">
-                    <GlassSearchBar onSearch={handleSearch} />
-                </div>
-            </main>
-
-            <div className="scroll-indicator">
-                <ChevronDown size={32} />
-            </div>
+  return (
+    <div className="home-page">
+      <video className="videoTag" autoPlay loop muted>
+        <source src={bgvideo} type="video/mp4" />
+      </video>
+      <VerticalNav onNavigate={handleNavigation} />
+      <main className="home-content">
+        <h1 className="header-home">Where do you want to go next ?</h1>
+        <div className="search-container">
+          <GlassSearchBar onSearch={handleSearch} />
         </div>
-    );
+      </main>
+      <div className="scroll-indicator">
+        <ChevronDown size={32}/>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
