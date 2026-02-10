858d3f75e436bf2cc07e80ba33862d6a486321f1
alihttps <aliaitbih02@gmail.com>
2026-02-10 20:09:22 +0100

    add

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProfilesModule } from './profiles/profiles.module'
+import { ProfilesController } from './profiles/profiles.controller';
+import { ProfilesService } from './profiles/profiles.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
+  controllers: [ProfilesController], // Must be here
+  providers: [ProfilesService],      // Must be here
 })
 export class AppModule {}
