36ff7ceb159b4efd790b81d60e854c04885e9b9b
alihttps <aliaitbih02@gmail.com>
2026-02-10 21:38:27 +0100

    adding an auth module, file structure no business logic yet

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,13 @@ import { ProfilesModule } from './profiles/profiles.module'
 import { ProfilesController } from './profiles/profiles.controller';
 import { ProfilesService } from './profiles/profiles.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [],
-  controllers: [ProfilesController], // Must be here
-  providers: [ProfilesService],      // Must be here
+  imports: [AuthModule, ProfilesModule],
+  // controllers: [AuthController, ProfilesController], 
+  // providers: [AuthService ,ProfilesService],  
 })
 export class AppModule {}
