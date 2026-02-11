b4f8a2f971fa05fb2a804805f96f35c4e4331b9c
samurai0lava <ouhsseineilyass@gmail.com>
2026-02-11 11:52:57 +0100

    chage the structure of frontend

--- frontend/src/features/RegisterPage.tsx
+++ frontend/src/features/RegisterPage.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import "./RegisterPage.css";
+import SigninOuth from "../components/shared/SigninOuth";
 import passportOverlay from '../assets/PassportOverlay.png';
 import RegisterForm from '../components/RegisterForm';
 import GlassCard from '../components/glassCard';
