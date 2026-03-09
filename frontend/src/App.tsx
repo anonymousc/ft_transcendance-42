2497fc50acf8a68f5cac4018d4cd72678932a4f3
samurai0lava <ouhsseineilyass@gmail.com>
2026-03-09 01:18:55 +0000

    init city finder feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import ProtectedRoute from './components/shared/ProtectedRoute';
 import Webchat from './features/chat/components/Webchat.tsx';
 import FriendsPage from './features/friends/components/FriendsPage';
 import NotificationPage from './features/notifications/NotificationPage';
+import City from './pages/City';
 
 function AppContent() {
   const location = useLocation();
@@ -31,6 +32,7 @@ function AppContent() {
         <Route path="/webchat" element={<Webchat />} />
         <Route path="/friends" element={<ProtectedRoute><FriendsPage /></ProtectedRoute>} />
         <Route path="/notifications" element={<ProtectedRoute><NotificationPage /></ProtectedRoute>} />
+        <Route path="/city" element={<ProtectedRoute><City /></ProtectedRoute>} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
---
+++ frontend/src/pages/City.tsx
@@ -0,0 +1,13 @@
+import GlassNavBar from "@/components/shared/GlassNavBar";
+
+function City() {
+  return (
+    <div>
+      <GlassNavBar />
+      <h1>City</h1>
+    </div>
+  );
+}
+
+
+export default City;
