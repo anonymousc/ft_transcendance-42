3a9e9d3463c709de98323bc59ab06b8d5d4ce308
autopsy1 <aessadik@c4r5p6.1337.ma>
2026-03-30 01:35:05 +0100

    update : fixing the time duration for the profiles service

--- backend/src/planner-service/src/server.js
+++ backend/src/planner-service/src/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT_PLAN || 7000;
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: 'http://localhost:5173',
   credentials: true,
 }));
 app.use(express.json());
